cdc5fa6f8c699a16dcad81b3c4d2c534be6890bd
jacksparrowoffice <swatipchhabra@gmail.com>
2026-08-19 19:49:04 +0530

    Format images configuration and set basePath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/a_to_zshopping',
 };
 
 module.exports = nextConfig;
+
